3c39a9fc417e7ef5bb805f474c521466d4db6d76
Cleverton <clevertonoliveira99@gmail.com>
2022-02-12 19:18:56 -0300

    Gerenciando estado da aplicação byteBank, commit 1

---
+++ lib/screens/dashboard/dashboard.dart
@@ -0,0 +1,27 @@
+import 'package:bytebank/models/saldo.dart';
+import 'package:bytebank/screens/dashboard/saldo.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class Dashboard extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Bytebank'),
+        ),
+        body: ListView(children: <Widget>[
+          Align(
+            alignment: Alignment.topCenter,
+            child: SaldoCard(),
+          ),
+          Consumer<Saldo>(builder: (context, saldo, child) {
+            return ElevatedButton(
+              child: Text('Adiciona'),
+              onPressed: () {
+                saldo.adiciona(17);
+              },
+            );
+          })
+        ]));
+  }
+}
